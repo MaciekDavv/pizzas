871b071fbc3a9b1cab8b9e7969f438b1a82810f2
MaciekDavv <dmaciek85@gmail.com>
2024-11-15 12:45:58 +0100

    add a SRC_TEST_LIST to the vatiabel, and this variable to the binary with tests

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -11,7 +11,7 @@ using namespace ::testing;
 struct PizzeriaTest : public ::testing::Test
 {
 public:
-    Pizzeria pizzeria = Pizzeria("dummyName"); 
+    Pizzeria pizzeria = Pizzeria("dummyName");
 };
 
 
